dd3607dc575d5ae922a49d9699dfb0711b02bd78
Ty Reid <trawx11@gmail.com>
2022-05-07 16:00:59 -0400

    working solution ot counting vowels in a string

---
+++ functions-problems/solution2.js
@@ -0,0 +1,27 @@
+
+
+
+
+function vowelCount(str) {
+
+// vowels
+const vowels = ['a','e','i','o','u'];
+// starting vowel count
+let count = 0;
+// splitting each letter and lowercasing
+const arrayOfLetters = str.toLowerCase().split('');
+
+// loop through string to test for vowel
+// lowers case to split each letter
+    for (let x = 0; x < arrayOfLetters.length; x++) {
+        if (vowels.includes(arrayOfLetters[x])) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+// take input
+
+console.log(vowelCount("how many vowels"));
