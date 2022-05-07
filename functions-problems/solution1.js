ecaaec1d08d2e8d66acc491d6b7cd8be0f65e4bd
Ty Reid <trawx11@gmail.com>
2022-05-06 21:34:08 -0400

    added comments to explain code

--- functions-problems/solution1.js
+++ functions-problems/solution1.js
@@ -1,7 +1,10 @@
+// writing a function for comparing two word lengths
+
 function longer(word, wordy){
 	return (wordy.length > word.length) ? wordy : word;
 }
 
+// function for splitting up each word for length analysis
 function longestWord(string) {
 	let words = string.split(" ");
 	return words.reduce(longer);
