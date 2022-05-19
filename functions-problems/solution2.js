48a3d39d13a698083bb49f70cee83b8c690cfb08
Ty Reid <trawx11@gmail.com>
2022-05-19 16:05:14 -0400

    modified solution with more comment explanations and syntax clean up

--- functions-problems/solution2.js
+++ functions-problems/solution2.js
@@ -3,17 +3,16 @@
 
 
 function vowelCount(str) {
+    // vowels
+    const vowels = ['a','e','i','o','u'];
+    // starting vowel count
+    let count = 0;
+    // splitting each letter and lowercasing
+    const arrayOfLetters = str.toLowerCase().split('');
 
-// vowels
-const vowels = ['a','e','i','o','u'];
-// starting vowel count
-let count = 0;
-// splitting each letter and lowercasing
-const arrayOfLetters = str.toLowerCase().split('');
-
-// loop through string to test for vowel
-// lowers case to split each letter
-    for (let x = 0; x < arrayOfLetters.length; x++) {
+        // loop through string to test for vowel
+    // lowers case to split each letter
+    for (let x = 0; x < arrayOfLetters.length; x ++) {
         if (vowels.includes(arrayOfLetters[x])) {
             count += 1;
         }
