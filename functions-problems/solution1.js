2fbb8786e9f9461e8501cb56b9304af68499307c
Ty Reid <trawx11@gmail.com>
2022-05-19 16:04:20 -0400

    modified solution with morecomments

--- functions-problems/solution1.js
+++ functions-problems/solution1.js
@@ -1,7 +1,9 @@
 // writing a function for comparing two word lengths
 
-function longer(word, wordy){
-	return (wordy.length > word.length) ? wordy : word;
+function longer(word1, word2, word3){
+	console.log(word1, word2, word3);
+	return (word2.length > word1.length) ? wordy : word;
+	
 }
 
 // function for splitting up each word for length analysis
@@ -11,3 +13,25 @@ function longestWord(string) {
 }
 
 console.log(longestWord("Shawshank Redemption"));
+
+
+
+function findLongestWord(str) {
+	const arrayOfWords = str.split(" ");
+  
+	//you can use an empty string "" bc the length of it is 0,
+	//or you can use the first word, or any random word in the arrayOfWords.
+  
+	//point is, you need a default reference point for your loop to compare with
+	//we need a value here to start with so we can use it to compare in our loop.
+  
+	let longestWord = "";
+  
+	for(let i = 0 ; i < arrayOfWords.length ; i++) {
+	  if(arrayOfWords[i].length > longestWord.length) {
+		longestWord = arrayOfWords[i];
+	  } 
+	}
+  
+	return longestWord;
+  }
