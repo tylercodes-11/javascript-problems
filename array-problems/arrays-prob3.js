8245a1dd06cbcf06f74423d57df794355e75bed1
Ty Reid <trawx11@gmail.com>
2022-05-25 23:05:21 -0400

    initial commit of prob 3 solution using sort method

---
+++ array-problems/arrays-prob3.js
@@ -0,0 +1,7 @@
+/* Problem 3: 
+Define a function that takes in an array of numbers/integers and returns the sorted version of that array, greatest to least
+*/
+
+const myArray = [-3,8,7,6,5,-4,3,2,1];
+//sort numbers in ascending order 
+myArray.sort(function(a, b){return a - b});
